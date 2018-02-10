1f12d60d3e6514144863e6eca6aa938d6ca6c7d6
Jonas Toth <jonas.toth@gmail.com>
2018-02-10 19:45:44 +0100

    [Feature] implement getters in sudoku

--- src/naive/sudoku.h
+++ src/naive/sudoku.h
@@ -5,15 +5,19 @@
 #include <array>
 
 namespace naive {
+
 class Sudoku {
 public:
     Sudoku() noexcept = default;
-
     ~Sudoku() noexcept = default;
+
+    Cell cellAt(uint8_t row, uint8_t col) const;
+    uint8_t valueAt(uint8_t row, uint8_t col) const;
 
 private:
     std::array<Cell, 81> __field; //< all value of the field, row-wise.
 };
+
 } /* naive */
 
 #endif /* end of include guard: SUDOKU_H_5WQRDNWG */
