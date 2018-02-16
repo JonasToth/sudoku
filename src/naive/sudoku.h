cdf93eb35a69bc85631c08f76a13556b12d7f841
Jonas Toth <jonas.toth@gmail.com>
2018-02-16 15:53:20 +0100

    [Feature] implement access methods for rows, cols and squares

--- src/naive/sudoku.h
+++ src/naive/sudoku.h
@@ -6,6 +6,12 @@
 
 namespace naive {
 
+/** Describe a sudoku game field.
+ *
+ * - Indices start with 1 and end with 9.
+ * - Each cell encodes its value, 0 if there is no value and tracks possible
+ *   values for the cell.
+ */
 class Sudoku {
 public:
     Sudoku() noexcept = default;
@@ -13,6 +19,15 @@ public:
 
     Cell cellAt(uint8_t row, uint8_t col) const;
     uint8_t valueAt(uint8_t row, uint8_t col) const;
+
+    std::array<uint8_t, 9> row(uint8_t row) const;
+    std::array<uint8_t, 9> col(uint8_t col) const;
+
+    /// Square numbering starts with 1 and is row wise.
+    /// 1 - 2 - 3
+    /// 4 - 5 - 6
+    /// 7 - 8 - 9
+    std::array<uint8_t, 9> square(uint8_t square) const;
 
 private:
     std::array<Cell, 81> __field; //< all value of the field, row-wise.
--- test/naive/test_sudoku.cpp
+++ test/naive/test_sudoku.cpp
@@ -22,6 +22,16 @@ TEST(sudoku, valueAt_dummy)
     }
     ASSERT_TRUE(all_zero);
 }
+TEST(sudoku, valueAt_exception)
+{
+    Sudoku s;
+    ASSERT_THROW(s.valueAt(0, 0), std::invalid_argument);
+    ASSERT_THROW(s.valueAt(2, 0), std::invalid_argument);
+    ASSERT_THROW(s.valueAt(0, 2), std::invalid_argument);
+    ASSERT_THROW(s.valueAt(10, 10), std::invalid_argument);
+    ASSERT_THROW(s.valueAt(10, 9), std::invalid_argument);
+    ASSERT_THROW(s.valueAt(9, 10), std::invalid_argument);
+}
 
 TEST(sudoku, cellAt_dummy)
 {
@@ -37,6 +47,86 @@ TEST(sudoku, cellAt_dummy)
     }
     ASSERT_TRUE(all_zero);
 }
+TEST(sudoku, cellAt_exception)
+{
+    Sudoku s;
+    ASSERT_THROW(s.cellAt(0, 0), std::invalid_argument);
+    ASSERT_THROW(s.cellAt(2, 0), std::invalid_argument);
+    ASSERT_THROW(s.cellAt(0, 2), std::invalid_argument);
+    ASSERT_THROW(s.cellAt(10, 10), std::invalid_argument);
+    ASSERT_THROW(s.cellAt(10, 9), std::invalid_argument);
+    ASSERT_THROW(s.cellAt(9, 10), std::invalid_argument);
+}
+
+TEST(sudoku, row_dummy)
+{
+    Sudoku s;
+    for(int i = 1; i <= 9; ++i) {
+        const auto row = s.row(i);
+        bool all_zero = true;
+
+        for(const auto value: row) {
+            if(value != 0)
+                all_zero = false;
+            EXPECT_EQ(value, 0);
+        }
+        ASSERT_TRUE(all_zero);
+    }
+}
+TEST(sudoku, row_exception)
+{
+    Sudoku s;
+
+    ASSERT_THROW(s.row(0), std::invalid_argument);
+    ASSERT_THROW(s.row(10), std::invalid_argument);
+}
+
+TEST(sudoku, col_dummy)
+{
+    Sudoku s;
+    for(int i = 1; i <= 9; ++i) {
+        const auto col = s.col(i);
+        bool all_zero = true;
+
+        for(const auto value: col) {
+            if(value != 0)
+                all_zero = false;
+            EXPECT_EQ(value, 0);
+        }
+        ASSERT_TRUE(all_zero);
+    }
+}
+TEST(sudoku, col_exception)
+{
+    Sudoku s;
+
+    ASSERT_THROW(s.col(0), std::invalid_argument);
+    ASSERT_THROW(s.col(10), std::invalid_argument);
+}
+
+TEST(sudoku, square_dummy)
+{
+    Sudoku s;
+    for(int i = 1; i <= 9; ++i) {
+        const auto square = s.square(i);
+        bool all_zero = true;
+
+        for(const auto value: square) {
+            if(value != 0)
+                all_zero = false;
+            EXPECT_EQ(value, 0);
+        }
+        ASSERT_TRUE(all_zero);
+    }
+}
+TEST(sudoku, square_exception)
+{
+    Sudoku s;
+
+    ASSERT_THROW(s.square(0), std::invalid_argument);
+    ASSERT_THROW(s.square(10), std::invalid_argument);
+}
+
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
